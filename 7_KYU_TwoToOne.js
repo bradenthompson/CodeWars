11271495086b2100f51e0f58ef9c0070e7fbc9a2
bradenthompson <bjt.codes@gmail.com>
2022-08-29 23:17:00 -0400

    daily codewars

---
+++ 7_KYU_TwoToOne.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+// 
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+// 
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    let mix = s1.concat(s2)
+    let mixed = mix.split('').sort()
+    let unique = mixed.filter(function(elem, index, self) {
+      return index === self.indexOf(elem)
+    })
+    return unique.join('')
+  }
