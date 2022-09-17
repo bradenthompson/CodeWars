58be56ea788e46699ae82922dcb9fa85a8f66679
bradenthompson <bjt.codes@gmail.com>
2022-09-16 23:48:58 -0400

    daily codewars

---
+++ 8_KYU_DefineCard.js
@@ -0,0 +1,22 @@
+// DESCRIPTION:
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+// 
+// Our deck (is preloaded):
+// 
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+    if (card.charAt(card.length-1) == '♣'){
+      return 'clubs'
+    }
+    else if (card.charAt(card.length-1) == '♠'){
+      return 'spades'
+    }
+      else if (card.charAt(card.length-1) == '♦'){
+      return 'diamonds'
+    }
+    else return 'hearts'
+    }
