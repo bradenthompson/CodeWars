c1ecf216443fb5c38076ac117278aeb0ff477477
bradenthompson <bjt.codes@gmail.com>
2022-11-10 23:45:33 -0500

    daily codewars

---
+++ 8_KYU_RemoveExclamation.js
@@ -0,0 +1,7 @@
+// Description
+// 
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/gi, '');
+  }
