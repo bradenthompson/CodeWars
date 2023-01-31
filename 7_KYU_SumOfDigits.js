08eed11877e2a7850d8ad19fd691b8df9f51e75d
bradenthompson <bjt.codes@gmail.com>
2023-01-31 16:47:42 -0500

    daily codewars

---
+++ 7_KYU_SumOfDigits.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Debug   function getSumOfDigits that takes positive integer to calculate sum of its digits. Assume that argument is an integer.
+// 
+// Example
+// 123  => 6
+// 223  => 7
+// 1337 => 14
+
+function getSumOfDigits(integer) {
+    let nums = integer.toString().split('').reduce((a,b) => parseInt(a)+parseInt(b),0)
+    return nums
+      }
