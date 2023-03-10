7e8ac65dd011c15cd7c165ffba4fe8af7b9fd87c
bradenthompson <bjt.codes@gmail.com>
2023-03-10 08:01:35 -0500

    daily codewars

---
+++ 8_KYU_SquaredSum.js
@@ -0,0 +1,11 @@
+// Description:
+// 
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// 
+// For example, for [1, 2, 2] it should return 9
+
+function squareSum(numbers){
+    let sum = 0;
+    numbers.forEach(num => sum += Math.pow(num,2))
+    return sum;
+  }
